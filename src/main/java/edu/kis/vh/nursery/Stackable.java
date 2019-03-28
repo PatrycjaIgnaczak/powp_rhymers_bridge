0b2a88db03ad7673bc9547379a41f1c8e98073aa
PatrycjaIgnaczak <buszasta@gmail.com>
2019-03-28 15:29:12 +0100

    3.1.4 add Stackable interface

---
+++ src/main/java/edu/kis/vh/nursery/Stackable.java
@@ -0,0 +1,14 @@
+package edu.kis.vh.nursery;
+
+public interface Stackable {
+
+    void countIn(int in);
+
+    boolean callCheck();
+
+    boolean isFull();
+
+    int peekaboo();
+
+    int countOut();
+}
