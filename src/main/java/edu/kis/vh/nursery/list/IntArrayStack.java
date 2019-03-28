6894373f3930d49c45315b7a1c0038d3bf7fb4b8
PatrycjaIgnaczak <buszasta@gmail.com>
2019-03-28 15:40:38 +0100

    3.1.6 move IntArrayStack to list package

--- src/main/java/edu/kis/vh/nursery/list/IntArrayStack.java
+++ src/main/java/edu/kis/vh/nursery/list/IntArrayStack.java
@@ -1,15 +1,20 @@
-package edu.kis.vh.nursery;
+package edu.kis.vh.nursery.list;
 
-public class IntArrayStack {
+import edu.kis.vh.nursery.Stackable;
+
+public class IntArrayStack implements Stackable {
     private int[] numbers = new int[12];
 
     public int total = -1;
 
+
+    @Override
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    @Override
     public boolean callCheck() {
         return total == -1;
     }
@@ -18,12 +23,14 @@ public class IntArrayStack {
         return total == 11;
     }
 
-    protected int peekaboo() {
+    @Override
+    public int peekaboo() {
         if (callCheck())
             return -1;
         return numbers[total];
     }
 
+    @Override
     public int countOut() {
         if (callCheck())
             return -1;
